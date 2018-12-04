6ce17c5c74f738be8159fa6524cfdbc64ba5cf33
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2018-12-04 18:42:31 +0700

    revision foder

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home|Andalucia</title>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">    
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="super_container">
+            <div class="page-header">
+            <!-- logo -->
+              <div class="logo-container">
+                <div class="logo">
+                    <img src="images/logo.jpeg" class="img-responsive" alt="Image">
+                    <span>Andalucia</span>                 
+                </div>
+              </div>
+                <ul class="nav justify-content-center|justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Active link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
--- login.php
+++ login.php
@@ -1,49 +1,40 @@
+<?php
 
+?>
 <!DOCTYPE html>
 <html lang="">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Title Page</title>
-
-        <!-- Bootstrap CSS -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        <title>Login to Andalucia</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1 class="text-center">Andalucia</h1>
+        <div class="loginmodal">
+        <h1 class="text-center"><img src="images/logo.jpeg" width=100px; height=100px;> Andalucia Study Center</h1>
         <form action="" method="POST" class="form-horizontal" role="form">
                 <div class="form-group">
-                    <label for="input" class="col-sm-2 control-label">Username</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="" id="username" class="form-control" value="" required="required" pattern="" title="">
+                    <label for="input" class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-6">
+                            <input type="email" name="" id="input" class="form-control" value="" required="required" title="">
                         </div>
                 </div>
                 
                 <div class="form-group">
                     <label for="input" class="col-sm-2 control-label">Password</label>
-                    <div class="col-sm-10">
+                    <div class="col-sm-6">
                         <input type="password" name="" id="password" class="form-control" required="required" title="">
                     </div>
                 </div>
                 
                 <div class="form-group">
-                    <div class="col-sm-10 col-sm-offset-2">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="col-sm-3 col-sm-offset-2">
+                        <button type="submit" class="btn btn-primary">Log in</button>
                     </div>
                 </div>
         </form>
-        
-        <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Bootstrap JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        </div>
     </body>
 </html>
